f5a8640defe0b4c727cbdaaaadf8ab9c179f8851
FSOFT.FPT.VN\KhiemDM <mrkhiem97@gmail.com>
2018-04-27 15:19:38 +0900

    Fix Url

--- CarClient/src/api/ApiUrl.js
+++ CarClient/src/api/ApiUrl.js
@@ -1,6 +1,6 @@
 const ApiUrl = {
     videoHost: 'http://localhost:51080/api/Video/PlayVideoAsync?filename=',
-    videoApi: 'localhost:51080/api/Video'
+    videoApi: 'http://localhost:51080/api/Video'
 }
 
 export default ApiUrl;
